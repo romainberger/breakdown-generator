11268227eab5da1bc5ed39b4b4375ca61b9f377c
romainberger <romain@romainberger.com>
2013-08-26 23:50:02 +0200

    Automatically output the riff on generation

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
     , playButton = document.querySelector('#play')
     , loading    = document.querySelector('#loading')
     , generateRiff = document.querySelector('#generate-riff')
-    , getRiff = document.querySelector('#get-riff')
     , tempoInput = document.querySelector('#tempo')
     , riffOutputWrapper = document.querySelector('#riff-output-wrapper')
     , riffOutput = document.querySelector('#riff-output')
@@ -32,19 +31,16 @@
     // generate riff
     generateRiff.addEventListener('click', function() {
       playButton.style.display = 'block'
-      getRiff.style.display = 'block'
       generator.generateRiff.call(generator)
+
+      // output the riff
+      var riff = generator.getJson.call(generator)
+      riffOutput.textContent = riff
     })
 
     // and play!
     playButton.addEventListener('click', function() {
       generator.play.call(generator)
-    })
-
-    // get the riff as json
-    getRiff.addEventListener('click', function() {
-      var riff = generator.getJson.call(generator)
-      riffOutput.textContent = riff
     })
 
     // load a riff
