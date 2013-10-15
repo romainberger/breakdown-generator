f8ef57946f8450f4ff52b8e35b68e3eb73a8867b
romainberger <romain@romainberger.com>
2013-10-15 22:23:27 +0200

    Remove tds to redraw the graph

--- js/breakdown-generator-graph.js
+++ js/breakdown-generator-graph.js
@@ -9,13 +9,14 @@
    * @param string table - Id of the element
    */
   var BreakdownGeneratorGraph = function(table) {
-    this.table    = document.querySelector(table)
+    this.table  = document.querySelector(table)
     this.china  = this.table.querySelector('#china')
     this.snare  = this.table.querySelector('#snare')
     this.kick   = this.table.querySelector('#kick')
     this.guitar = this.table.querySelector('#guitar')
     this.riff
-    this.instruments = ['snare', 'china', 'kick']
+    this.instruments    = ['snare', 'china', 'kick']
+    this.allInstruments = [this.china, this.snare, this.kick, this.guitar]
   }
 
   BreakdownGeneratorGraph.prototype = {
@@ -25,10 +26,14 @@
        */
       draw: function(riff) {
         this.riff = JSON.parse(riff)
+        this.empty()
         this.drawGuitar()
         this.drawInstruments()
       }
 
+      /**
+       * Generate the basic instruments' graph
+       */
     , drawInstruments: function() {
         var self = this
 
@@ -46,8 +51,34 @@
         })
       }
 
+      /**
+       * Generate the guitar's graph
+       */
     , drawGuitar: function() {
+        var self = this
+          , html = ''
 
+        for (var i = 0; i < 8; i++) {
+          html += '<td'
+          // the guitar follows the kick pattern so we check with it
+          if (self.riff.kick.indexOf(i) != 1) {
+            html += ' class="filled"'
+          }
+          html += '></td>'
+        }
+
+        self.guitar.insertAdjacentHTML('beforeend', html)
+      }
+
+      /**
+       * Remove the existing tds
+       */
+    , empty: function() {
+        this.allInstruments.forEach(function(element) {
+          [].forEach.call(element.querySelectorAll('td'), function(td) {
+            element.removeChild(td)
+          })
+        })
       }
 
   }
